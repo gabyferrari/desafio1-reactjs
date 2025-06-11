e773e959d2ec3948970bc608d5ee9c4256d68112
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-11 19:24:15 -0300

    Case responsiveness and finishing project

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function Footer() {
 
     return (
-        <div className="dsct-container footer-content">
+        <div className="dsct-container dsct-footer-content">
             <a href="#">@dscarrostop</a>
             <address>Rua da Pipoca, 150</address>
         </div>
--- src/routes/MainPage/index.tsx
+++ src/routes/MainPage/index.tsx
@@ -1,14 +1,15 @@
+import './styles.css'
 import Header from "../../components/Header";
 import CarCard from "../../components/CarCard";
 import Comments from "../../components/Comments";
 import Footer from "../../components/Footer";
 
-export default function App() {
+export default function MainPage() {
 
   return (
     <>
       <Header />
-      <main className="dsct-page-main">
+      <main>
         <section id="car-card-section">
           <CarCard /> 
         </section>
